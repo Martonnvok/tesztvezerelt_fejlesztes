5a40871c806c56d810bdc4557968ba2e0f8dfac4
Martonnvok <martonsom@gmail.com>
2023-03-24 08:28:53 +0100

    késleltetések  a javascriptben

---
+++ memoria/main.js
@@ -0,0 +1,14 @@
+$(function () {
+    console.log("betöltődés után")
+    setTimeout(function(){
+        console.log("1000 ms múlva")
+    },1000)
+    console.log("setTimeOut után")
+    let i = 0;
+    
+   
+
+})
+
+console.log("betöltődés után")
+
